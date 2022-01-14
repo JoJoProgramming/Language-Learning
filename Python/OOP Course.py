104454c35911ca931ae24290b54187f34e1e8b21
JoJo <jpollockenterprise@gmail.com>
2022-01-13 18:02:06 -0600

    End for the night, document progress.

--- Python/OOP Course.py
+++ Python/OOP Course.py
@@ -1,7 +1,8 @@
 class Item:
     #__init__ method is called automatically when an instance of the class is built
     #parameters given to init are mandatory. parameter variables can be set during initialization
-    #as seen with quantity=0 in the argument line.
+    #as seen with quantity=0 in the argument line. preassigned arguments will override when an instance
+    #is assigned that value; EG item 1 quantity will always be 5 once assigned.
     def __init__(self, name, price, quantity=0):
         #creates name attribute for the created instance and assigns it to the variable passed into
         #the init method via argument name
@@ -19,10 +20,4 @@ item1 = Item("Phone",100)
 phoneQuantity = input("How many phones do you want? ")
 phoneQuantity = int(phoneQuantity)
 item1.quantity = phoneQuantity
-item2 = Item("Laptop",1000,3)
-print(item1.name)
-print(item2.name)
-print(item1.quantity)
-print(item2.quantity)
-print(item1.price)
-print(item2.price)
+print(item1.calculate_total_price(item1.price,item1.quantity))
