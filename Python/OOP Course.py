620ba3a89017656cd01241b93bc65b6842eb8913
JoJo <jpollockenterprise@gmail.com>
2022-01-14 08:43:57 -0600

    Updated code and notes via Google Doc - Python Examples

--- Python/OOP Course.py
+++ Python/OOP Course.py
@@ -1,23 +1,18 @@
 class Item:
-    #__init__ method is called automatically when an instance of the class is built
-    #parameters given to init are mandatory. parameter variables can be set during initialization
-    #as seen with quantity=0 in the argument line. preassigned arguments will override when an instance
-    #is assigned that value; EG item 1 quantity will always be 5 once assigned.
-    def __init__(self, name, price, quantity=0):
-        #creates name attribute for the created instance and assigns it to the variable passed into
-        #the init method via argument name
+    def __init__(self, name: str, price: float, quantity: int = 0):
+        #validations for received arguments
+        assert price >= 0
+        assert quantity >= 0
+        #assign to self instance
         self.name = name
         self.price = price
         self.quantity = quantity
-    #the self in the below method refers to the object itself being passed into the method;
-    #EG item1 is passed into method calculate_total_price as the first argument
-    #self must be included for methods in a classs
-    def calculate_total_price(self, x, y):
-        return x * y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 item1 = Item("Phone",100)
-phoneQuantity = input("How many phones do you want? ")
-phoneQuantity = int(phoneQuantity)
-item1.quantity = phoneQuantity
-print(item1.calculate_total_price(item1.price,item1.quantity))
+item1Quant = input("How many phones do you want? ")
+item1Quant = int(item1Quant)
+item1.quantity = item1Quant
+print(item1.calculate_total_price())
