75b16742550cd5b530c37c0f8e49eda4de8ea38c
JoJo <jpollockenterprise@gmail.com>
2022-01-14 09:31:40 -0600

    Overwriting object attributes and running methods on new values example

--- Python/OOP Course.py
+++ Python/OOP Course.py
@@ -1,18 +1,27 @@
 class Item:
-    def __init__(self, name: str, price: float, quantity: int = 0):
+    pay_rate = 0.8 #item cost after 20% discount
+    def __init__(self, name: str = "", price: float = 0, quantity: int = 0):
         #validations for received arguments
-        assert price >= 0
-        assert quantity >= 0
+        assert price >= 0, f"Price {price} is not greater than 0!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than 0!"
         #assign to self instance
         self.name = name
         self.price = price
         self.quantity = quantity
+    def determine_price(self):
+        if self.name == "Phone":
+            self.price = 100
     def calculate_total_price(self):
-        return self.price * self.quantity
+        self.price = self.price * self.quantity
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
 
 
-item1 = Item("Phone",100)
-item1Quant = input("How many phones do you want? ")
-item1Quant = int(item1Quant)
-item1.quantity = item1Quant
-print(item1.calculate_total_price())
+item1 = Item()
+item1.name = input("What do you want to buy? ")
+item1.determine_price()
+item1.quantity = int(input("How many phones do you want to buy? "))
+item1.calculate_total_price()
+item1.apply_discount()
+print(item1.price)
+
